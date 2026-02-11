9e478fae61f4e4d5d5cdd49252d620b2196a95de
HHHOUBOHAN <3420579752@qq.com>
2026-02-11 22:52:24 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,66 @@
-//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
-// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
-void main() {
-    //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
-    // 查看 IntelliJ IDEA 建议如何修正。
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
-        // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
-        IO.println("i = " + i);
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//    }
+//}
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello world");
+//        System.out.println("YYDS");
+//        System.out.println("YYDS");
+//    }
+//}
+//public class Main {
+//    static void main(String[] args) {
+//        System.out.println("Hello World");
+//    }
+//}
+//void main{
+//    System.out println("Hello");
+//
+//        }
+//注释 // /**/  /**  ///（Markdown 格式）
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//    }
+//}
+//    /**
+//     * @since 2025
+//     * @解放拉萨就
+//     */
+//变量与常量
+//public class Main {
+//    public static void main(String[] args) {
+//        int a = 10, b = 100;
+//        a=100;
+//        final int m=100;
+// //       常量
+//        System.out.println(a);
+//        System.out.println(a+1);
+// //        变量名要有意义
+//        System.out.println("Hello World");
+//    }
+//}
+//数据类型
+//8bit=1byte
+//整数类型 byte short(16bit) int(32bit) long(64bit)
+//public class Main {
+//    public static void main(String[] args) {
+//        byte b=34;
+//        short s=10000;
+//        int i=2000000000;
+//        long l=10000000000L;
+// //        加 L 是在告诉编译器：这是一个 long 类型的数字
+//    }
+//}
+//存储类型从小的整数类型转换成大的整数类型就会发生隐式类型转换
+//十六进制以0x开头的都是十六进制表示法   八进制 以0开头的都是八进制 012=10 0112=72
+public class Main {
+    public static void main(String[] args) {
+        int a=2147483647;
+        a=a+1;
+        System.out.println(a);
     }
 }
+
